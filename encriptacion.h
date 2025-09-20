f97dd0136d831a2ac13c42d029e68b25f7d8aa7a
juancamilo122006 <juanc.ramirezl@udea.edu.co>
2025-09-20 16:48:28 -0500

    Update encriptacion.h

--- encriptacion.h
+++ encriptacion.h
@@ -1,4 +1,16 @@
 #ifndef ENCRIPTACION_H
 #define ENCRIPTACION_H
 
-#endif // ENCRIPTACION_H
+// Función para desencriptar un buffer aplicando
+// primero XOR con la clave y luego rotación a la derecha.
+// Parámetros:
+//   data     -> puntero al arreglo encriptado
+//   len      -> longitud del arreglo
+//   n_rotate -> cantidad de bits a rotar (1–7)
+//   key      -> clave XOR (1 byte)
+//
+// Retorna un nuevo puntero a un arreglo desencriptado
+// (el llamador debe liberar con delete[]).
+unsigned char* tryDecrypt(const unsigned char* data, int len, int n_rotate, unsigned char key);
+
+#endif // ENCRYPTION_H
